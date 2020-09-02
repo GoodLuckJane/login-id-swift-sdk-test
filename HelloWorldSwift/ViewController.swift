68258e36456c6caf7f6edbcf2f71fa8e58344ad4
GoodLuckJane <jane.zhengying.zhang@gmail.com>
2020-09-02 12:59:20 -0400

    remove the + sign in signup, all methods were tested and work fine

--- HelloWorldSwift/ViewController.swift
+++ HelloWorldSwift/ViewController.swift
@@ -31,35 +31,38 @@ class ViewController: UIViewController {
                 // get current token example
                 let token = LoginApi.client.getCurrentToken()
                 print("user is logged in, and the token is: \(token)")
+                print("Now log the user out to test logout and login")
+                LoginApi.client.logout()
             } else {
                 // can setup login user here
                 //present login option here
                 print("user is NOT logged in, log the user in now")
-                // LoginApi.client.login(onComplete: { (response) in
-                //     if(response.success){
-                //         // handle success case here
-                //     } else {
-                //         // handle error case here
-                //         print(response.errorMessage)
-                //     }
-                // })
+                 LoginApi.client.login(onComplete: { (response) in
+                     if(response.success){
+                         // handle success case here
+                        print("login success: \(response)")
+                     } else {
+                         // handle error case here
+                         print("login failed with error message: ",response.errorMessage)
+                     }
+                 })
             }
         } else {
             // user has no account so can setup register user logic here
             print("user doesn't have an account, register now")
-            LoginApi.client.register(username: "Jane+1", onComplete: { (response) in
+            LoginApi.client.register(username: "Jane1", onComplete: { (response) in
                 if(response.success){
                     // handle success case here
                     print("registered successfully!, the response is: ",response)
-                    // // ask user to relogin if successfully logged in
-                    // LoginApi.client.login(onComplete: { (response) in
-                    //     if(response.success){
-                    //         // handle success case here
-                    //     } else {
-                    //         // handle error case here
-                    //         print(response.errorMessage)
-                    //     }
-                    // })
+                     // ask user to relogin if successfully logged in
+                     LoginApi.client.login(onComplete: { (response) in
+                         if(response.success){
+                             // handle success case here
+                         } else {
+                             // handle error case here
+                             print(response.errorMessage)
+                         }
+                     })
                 } else {
                     // handle error case here
                     print("registration error message",response.errorMessage)
